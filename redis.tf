9553dfc754dd2131cd79a039f31832c5cb4baa15
CodingManoj <CodingManoj@gmail.com>
2024-01-09 06:20:41 +0530

    TF DB Auto Push

--- redis.tf
+++ redis.tf
@@ -22,36 +22,3 @@ resource "aws_elasticache_subnet_group" "redis" {
   name       = "roboshop-${var.ENV}-redis-subnet-group"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 }
-
-
-# Provisions Document DB Cluster
-# resource "aws_docdb_cluster" "docdb" {
-#   cluster_identifier      = "roboshop-${var.ENV}-docdb"
-#   engine                  = "docdb"
-#   master_username         = "admin1"
-#   master_password         = "roboshop1"
-# #   backup_retention_period = 5                             # Commented to avoid backups in lab
-# #   preferred_backup_window = "07:00-09:00"
-#   skip_final_snapshot     = true
-#   db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
-#   vpc_security_group_ids  = [aws_security_group.allows_docdb.id]
-# }
-
-
-# # Creates a subnet-groups
-# resource "aws_docdb_subnet_group" "docdb" {
-#   name       = "roboshop-${var.ENV}-docdb-subnet-group"
-#   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
-
-#   tags = {
-#     Name = "roboshop-${var.ENV}-docdb-subnet-group"
-#   }
-# }
-
-# # Creates DocDB Cluster Instances 
-# resource "aws_docdb_cluster_instance" "cluster_instances" {
-#   count              = 1
-#   identifier         = "roboshop-${var.ENV}-docdb"
-#   cluster_identifier = aws_docdb_cluster.docdb.id
-#   instance_class     = "db.t3.medium"
-}
