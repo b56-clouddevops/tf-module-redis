19d382d72dcba931cf5421b8bbda5f5f4df7b06d
CodingManoj <CodingManoj@gmail.com>
2024-01-21 21:00:15 -0500

    TF Backend Infra Auto Push

--- route53.tf
+++ route53.tf
@@ -9,3 +9,7 @@ resource "aws_route53_record" "redis_dns_record" {
 output "redis_op" {
   value  = aws_elasticache_cluster.redis
 }
+
+output "REDIS_ENDPOINT" {
+  value = aws_elasticache_cluster.redis.cache_nodes.0.address
+}
